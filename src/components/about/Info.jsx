307af33c1d0aa8fbee61fe714772873c2df7caf5
umitavc <umitavc62@gmail.com>
2024-01-11 15:38:25 +0300

    about  commponent aded

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+const Info = () => {
+    return(
+        <div className="about__info grid">
+            <div className="about__box">
+            <i class='bx bx-award about__icon'></i>
+                <h3 className="aboout__title"> Experience </h3>
+                <span className="about__subtitle"> 
+                        2 + Years 
+                    </span>
+            </div>
+
+            <div className="about__box">
+            <i class='bx bx-briefcase-alt-2 about__icon' ></i>
+                <h3 className="aboout__title">Completed </h3>
+                <span className="about__subtitle">
+                        31 + Projects
+                    </span>
+            </div>
+
+            <div className="about__box">
+            <i class='bx bx-support about__icon'></i>
+                <h3 className="aboout__title"> Support</h3>
+                <span className="about__subtitle">
+                        7/24
+                    </span>
+            </div>
+        </div>
+    )
+}
+export default Info;
